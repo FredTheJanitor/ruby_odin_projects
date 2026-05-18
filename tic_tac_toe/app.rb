f170f97a411c6ab72c1398b049e91a9cd9ade2db
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-05-18 18:30:00 -0400

    feat: create basic game logic and terminal outputs

---
+++ tic_tac_toe/app.rb
@@ -0,0 +1,5 @@
+require_relative 'lib/TerminalOutput'
+
+new_board = TerminalOutput.new
+new_board.print_board
+new_board.request_input
--- tic_tac_toe/lib/TerminalOutput.rb
+++ tic_tac_toe/lib/TerminalOutput.rb
@@ -1,3 +1,32 @@
 class TerminalOutput
+  attr_accessor :one, :two, :three, :four, :five, :six, :seven, :eight, :nine
+  def initialize
+    @one = 1
+    @two = 2
+    @three = 3
+    @four = 4
+    @five = 5
+    @six = 6
+    @seven = 7
+    @eight = 8
+    @nine = 9
+    @winner = "Nobody"
+  end
+  def print_board
+    puts "\e[4m#{@one}|#{@two}|#{@three}\n#{@four}|#{@five}|#{@six}\e[0m\n#{@seven}|#{@eight}|#{@nine}"
+  end
   
+  def request_input
+    puts "_______________________________"
+    puts "Pick a Number to Place a Marker"
+    puts "-------------------------------"
+  end
+
+  def game_over
+    puts "_______________________________"
+    puts "GAME OVER! #{@winner} wins!"
+    puts "-------------------------------"
+  end
+
+
 end
