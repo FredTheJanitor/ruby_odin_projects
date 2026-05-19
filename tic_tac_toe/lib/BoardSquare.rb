fcd9b3571b966a38e6f0af6f6cddbba2283fab10
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-05-19 18:30:00 -0400

    fix: change = to == in if condition and fix logic

--- tic_tac_toe/lib/BoardSquare.rb
+++ tic_tac_toe/lib/BoardSquare.rb
@@ -9,27 +9,41 @@ class BoardSquare
     if @@picked_boardsquares.include? 4
       four_win_condition1 = [1,4,7]
       four_win_condition2 = [4,5,6]
-      return 4 if (four_win_condition1 - @@picked_boardsquares).empty?
-      return 4 if (four_win_condition2 - @@picked_boardsquares).empty?
+      return 4 if (four_win_condition1 - @@x_list).empty?
+      return 4 if (four_win_condition1 - @@o_list).empty?
+
+      return 4 if (four_win_condition2 - @@x_list).empty?
+      return 4 if (four_win_condition2 - @@o_list).empty?
     end
     if @@picked_boardsquares.include? 2
       two_win_condition1 = [1,2,3]
       two_win_condition2 = [2,5,8]
-      return 2 if (two_win_condition1 - @@picked_boardsquares).empty?
-      return 2 if (two_win_condition2 - @@picked_boardsquares).empty?
+      return 2 if (two_win_condition1 - @@x_list).empty?
+      return 2 if (two_win_condition1 - @@o_list).empty?
+
+      return 2 if (two_win_condition2 - @@x_list).empty?
+      return 2 if (two_win_condition2 - @@o_list).empty?
     end
     if @@picked_boardsquares.include? 9
       nine_win_condition1 = [3,6,9]
       nine_win_condition2 = [1,5,9]
       nine_win_condition3 = [7,8,9]
-      return 9 if (nine_win_condition1 - @@picked_boardsquares).empty?
-      return 9 if (nine_win_condition2 - @@picked_boardsquares).empty?
-      return 9 if (nine_win_condition3 - @@picked_boardsquares).empty?
+      return 9 if (nine_win_condition1 - @@x_list).empty?
+      return 9 if (nine_win_condition1 - @@o_list).empty?
+
+      return 9 if (nine_win_condition2 - @@x_list).empty?
+      return 9 if (nine_win_condition2 - @@o_list).empty?
+
+      return 9 if (nine_win_condition3 - @@x_list).empty?
+      return 9 if (nine_win_condition3 - @@o_list).empty?
+
     end
     if @@picked_boardsquares.include? 7
       seven_win_condition = [7,5,3]
-      return 7 if (seven_win_condition - @@picked_boardsquares).empty?
+      return 7 if (seven_win_condition - @@x_list).empty?
+      return 7 if (seven_win_condition - @@o_list).empty?
     end
+    
   end
 
   def initialize(player, space_number)
@@ -38,8 +52,9 @@ class BoardSquare
     @@number_of_plays += 1
     @move_number = @@number_of_plays
     @@picked_boardsquares.push(space_number)
-    if player = "x"
+    if player == "x"
       @@x_list.push(space_number)
+
     else
       @@o_list.push(space_number)
     end
@@ -48,6 +63,8 @@ class BoardSquare
 
   def clear_board
     @@picked_boardsquares = []
+    @@x_list = []
+    @@o_list = []
   end
 
   def check_who_won(space)
