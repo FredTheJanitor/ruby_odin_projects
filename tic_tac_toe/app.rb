8701f6408e20f09aafb93012e8129e2b8ab162db
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-05-19 18:30:00 -0400

    fix: quirk with requesting user input

--- tic_tac_toe/app.rb
+++ tic_tac_toe/app.rb
@@ -6,13 +6,13 @@ require_relative 'lib/terminal_output'
 
 board = TerminalOutput.new
 board.print_board
-board.request_input
 game = GameCondition.new
 9.times do
+  board.request_input
   move = BoardSquare.new(game.turn, game.player_turn)
   board.change_board(move.player, move.space_number)
   board.print_board
-  board.request_input
+
   board.game_over(move.check_who_won(BoardSquare.check_for_wins)) if BoardSquare.check_for_wins
   game.next_player
 end
