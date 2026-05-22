103402bb0824cf4f78c4ebbee09535470defd0fa
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-05-22 18:30:00 -0400

    feat: add core code_maker_logic

---
+++ mastermind/lib/code_maker_logic.rb
@@ -0,0 +1,21 @@
+class CodeMakerLogic
+  def initialize
+    @secret_code = [rand(1..6),rand(1..6),rand(1..6),rand(1..6)]
+    @hint_keys = [0,0,0,0]
+  end
+  def check_guess_row(guess_row)
+    @hint_keys = []
+    guess_row.each_with_index { |guess, i|
+      if guess[i] == @secret_code[i]
+        @hint_keys.push(2)
+        next
+      end
+      if @secret_code.any? == guess[i] 
+        @hint_keys.push(1)
+        next
+      end
+      @hint_keys.push(0)
+    }
+    @hint_keys
+  end
+end
