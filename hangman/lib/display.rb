bccf982b4206bbd19dba81ab6043f23a6eb3058c
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-06-04 21:14:50 -0400

    feat: add some basic introductory text methods

--- hangman/lib/display.rb
+++ hangman/lib/display.rb
@@ -1,2 +1,20 @@
 class Display
+
+  def intro_text
+    puts "Welcome to Hangman Terminal\n---------------------------------"
+  end
+
+  def game_selection_text
+    puts "\tInstructions:"
+    puts "Select a saved game by its Number\nor hit 0 to start a new game"
+    puts '---------------------------------'
+  end
+
+  def game_start_text(word)
+    puts "\tNew Game!"
+    puts '_ ' * word.length
+    puts "type a letter and we'll tell you\nif its in the word"
+    puts '---------------------------------'
+  end
+
 end
