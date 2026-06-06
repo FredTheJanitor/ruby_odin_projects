2ad3a52c2f3dad3db15350ea12132c1ac2231f34
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-06-06 16:23:21 -0400

    chore: begin restructuring to use serialization

--- hangman/lib/game_play.rb
+++ hangman/lib/game_play.rb
@@ -1,2 +1,24 @@
+
+
 class GamePlay
+
+  # takes the output of random word picker
+  def initialize(selected_word)
+    @selected_word = selected_word
+    @guess_number = 0
+    @guess_wrong = 0
+    @guess_list = []
+    @board_blanks = []
+    @board_hits = []
+  end
+
+  def take_turn(guess)
+    @guess_number += 1
+    @guess_wrong += 1 unless @selected_word.include? guess
+    @guess_list.push guess
+  end
+
+
+
+
 end
