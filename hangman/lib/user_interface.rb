14183213dd0949930c14f682862ead627416d0e7
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-06-07 16:35:32 -0400

    chore: update syntax

--- hangman/lib/user_interface.rb
+++ hangman/lib/user_interface.rb
@@ -1,3 +1,12 @@
 class UserInterface
+  def initialize
+  end
+
+  def get_input
+    loop do 
+      new_guess = gets.chomp
+      return new_guess if new_guess.match?(/[a-zA-Z]/)
+    end
+  end
   
 end
